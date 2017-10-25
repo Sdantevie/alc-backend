3097ee14d08b439505b15e789ff67983c05558d1
Steven <dstevie9292@gmail.com>
2017-10-25 09:28:35 +0100

    fixing route.delete

--- app/routes/studentRouting.js
+++ app/routes/studentRouting.js
@@ -4,7 +4,7 @@ const student = require('../controllers/studentController.js');
 
 const routes = (app) => {
     app.route('/students').get(student.getStudents).post(student.postStudent);
-    app.route('/students/:id').get(student.getStudentById).post(student.updateStudent).delete(student.deleteStudent);
+    app.route('/students/:id').get(student.getStudentById).post(student.updateStudent).delete(student.deleteResource);
     app.route('/api/authenticate').post(student.authenticate);
     app.route('/students/get/:name').get(student.getStudentByName);
 }
